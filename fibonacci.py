f3364b4efd5a6d3032b259d14bd9cf3c926c5f9a
GuiHenriques <gui10ghc@gmail.com>
2022-12-04 00:39:58 -0300

    poo

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,14 @@
-# 1 2 3 4 5 6 7  8
-# 1 1 2 3 5 8 13 21 
-
-n = int(input())
+import sys
+if len(sys.argv) == 2:
+    n = int(sys.argv[1])
+else:
+    n = int(input("Number: "))
 
 a = 1
 b = 1
+print(a, b, sep="\n")
 for i in range(n-2):
     c = a + b
     a = b
     b = c
-print(b)
+    print(c)
